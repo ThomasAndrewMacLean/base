eb1e85520bcc6218b2b95b9d535fcab25e1f0509
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-01 11:28:39 +0000

    Deploy ThomasAndrewMacLean/base to github.com/ThomasAndrewMacLean/base.git:gh-pages

---
+++ precache-manifest.91569273ec81aaaea241c709e84007d6.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25705a875045c962ba5c",
+    "url": "app.bundle.js"
+  },
+  {
+    "revision": "7f2d861756b82bb4ffa84f6a27121fcd",
+    "url": "assets/favicon.ico"
+  },
+  {
+    "revision": "652d24a3ff8bff45f121260b198b4230",
+    "url": "index.html"
+  }
+]);
